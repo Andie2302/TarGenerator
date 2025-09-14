f4f935f25948144a44cbe9e186211df91261b42d
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:38 +0200

    Add TarReader.Async with async tar stream reading implementation

---
+++ TarGenerator/Core/TarReader.Async.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using TarGenerator.Core.Interfaces;
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+namespace TarGenerator.Core
+{
+    public partial class TarReader : ITarStreamReadAsync
+    {
+        public ValueTask<int> ReadAsync(byte[] buffer, int offset, int count) => new(BaseStream.ReadAsync(buffer, offset, count));
+    }
+}
+#endif
