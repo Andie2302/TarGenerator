f151b8c8570985b2e3b1f456b41edf46df1a6dc8
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:15 +0200

    Add ITarStreamWriteAsync interface for async tar stream writing

---
+++ TarGenerator/Core/Interfaces/ITarStreamWriteAsync.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace TarGenerator.Core.Interfaces;
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+public interface ITarStreamWriteAsync
+{
+    ValueTask WriteAsync(byte[] buffer, int offset, int count);
+    ValueTask FlushAsync();
+}
+#endif
