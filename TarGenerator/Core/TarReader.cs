c2d4a39f666f5dd88627ed515aeda8a723d82438
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:43 +0200

    Add TarReader class for tar stream reading implementation

---
+++ TarGenerator/Core/TarReader.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+using TarGenerator.Core.Interfaces;
+
+namespace TarGenerator.Core;
+
+public partial class TarReader(Stream baseStream) : TarBase(baseStream), ITarStreamRead
+{
+    public int Read(byte[] buffer, int offset, int count) => BaseStream.Read(buffer, offset, count);
+}
