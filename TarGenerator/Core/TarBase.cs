4ad867bcdd610276322d68fd40902506ca6dd05f
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:26 +0200

    Add abstract TarBase class with IDisposable implementation

---
+++ TarGenerator/Core/TarBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace TarGenerator.Core
+{
+    public abstract partial class TarBase(Stream baseStream) : IDisposable
+    {
+        protected readonly Stream BaseStream = baseStream ?? throw new ArgumentNullException(nameof(baseStream));
+        protected bool IsDisposed = false;
+
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (IsDisposed) return;
+            if (disposing) BaseStream.Dispose();
+            IsDisposed = true;
+        }
+    }
+}
