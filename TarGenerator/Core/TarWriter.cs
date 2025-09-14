52a5366a65da93fb1d76b9b1716ac4c4b19b2713
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:56 +0200

    Add TarWriter class for tar stream writing implementation

---
+++ TarGenerator/Core/TarWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using TarGenerator.Core.Interfaces;
+
+namespace TarGenerator.Core;
+
+public partial class TarWriter(Stream baseStream) : TarBase(baseStream), ITarStreamWrite
+{
+    public void Write(byte[] buffer, int offset, int count) => BaseStream.Write(buffer, offset, count);
+
+    public void Flush() => BaseStream.Flush();
+}
