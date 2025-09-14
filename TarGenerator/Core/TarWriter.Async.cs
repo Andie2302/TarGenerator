3a7681bbd8bf167c33c83aff2a5df29565fa24d7
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:49 +0200

    Add TarWriter.Async with async tar stream writing implementation

---
+++ TarGenerator/Core/TarWriter.Async.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using TarGenerator.Core.Interfaces;
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+namespace TarGenerator.Core
+{
+    public partial class TarWriter : ITarStreamWriteAsync
+    {
+        public ValueTask WriteAsync(byte[] buffer, int offset, int count) =>
+            new(BaseStream.WriteAsync(buffer, offset, count));
+
+        public ValueTask FlushAsync() => new(BaseStream.FlushAsync());
+    }
+}
+#endif
