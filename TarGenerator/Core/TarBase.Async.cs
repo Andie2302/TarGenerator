447870174813c3db3f45d615c88de265e9f3d952
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:20 +0200

    Add async disposable support to TarBase class

---
+++ TarGenerator/Core/TarBase.Async.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+namespace TarGenerator.Core
+{
+    public abstract partial class TarBase : IAsyncDisposable
+    {
+        public virtual async ValueTask DisposeAsync()
+        {
+            if (IsDisposed) return;
+            await BaseStream.DisposeAsync().ConfigureAwait(false);
+            IsDisposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+#endif
