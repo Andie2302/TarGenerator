999ca39d941487c0eab2eae39f39daba01bf3dbc
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 11:50:18 +0200

    Add TarTools class with ASCII encoding and Unix time method placeholder

---
+++ TarGenerator/Tools/TarTools.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace TarGenerator.Tools;
+
+public class TarTools
+{
+    public static Encoding Ascii { get; } = Encoding.GetEncoding("ASCII");
+
+    public static long FromUnixTimeSeconds(long readOctal)
+    {
+        throw new System.NotImplementedException();
+    }
+}
