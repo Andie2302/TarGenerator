b5f680d1016ee49043f503b0c57b3146343e98bd
Andreas <Andreas.Zwander@gmail.com>
2025-09-14 03:53:03 +0200

    Add ITarStreamReadAsync interface for async tar stream reading

---
+++ TarGenerator/Core/Interfaces/ITarStreamReadAsync.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+namespace TarGenerator.Core.Interfaces
+{
+    public interface ITarStreamReadAsync
+    {
+        ValueTask<int> ReadAsync(byte[] buffer, int offset, int count);
+    }
+}
+#endif
